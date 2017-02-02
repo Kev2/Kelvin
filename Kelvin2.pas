8514c4c761ddc7891d6d3b267250fea9974d3fd4
Norberto <nventafini@inbox.com>
2017-02-02 18:36:13 -0300

    Interface Updated

--- Kelvin2.pas
+++ Kelvin2.pas
@@ -10,14 +10,14 @@ uses
   Forms, ksplash, accel, angle, angvel, area, aphys,
   cdata, cdflow, cnum, cloop, dens, len, mass, temp, vol, echarge, cur,
   ind, rescon, volt, emr, work, flowld, force, fuelc, lux, magnf, mus, power,
-  prefx, shoe, press, speed, tque, visc, time, aboutk, maink;
+  prefx, shoe, press, speed, tque, visc, time, maink, aboutk;
 
 {$IFDEF WINDOWS}{$R Kelvin2.rc}{$ENDIF}
 
 {$R *.res}
 
 begin
-  Application.Title:='Chitwanix Kelvin';
+  Application.Title:='Kelvin';
   Application.Initialize;
   Application.CreateForm(Tkelvinm, kelvinm);
   Application.CreateForm(Tabk, abk);
