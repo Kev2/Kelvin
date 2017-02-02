c5e1a8908c5beeddfea9c2cf78f9286aa1d48687
Norberto <nventafini@inbox.com>
2017-02-02 17:10:00 -0300

    interface updated

--- Kelvin2.pas
+++ Kelvin2.pas
@@ -7,17 +7,17 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, maink, aboutk, ksplash, accel, angle, angvel, area, aphys,
+  Forms, ksplash, accel, angle, angvel, area, aphys,
   cdata, cdflow, cnum, cloop, dens, len, mass, temp, vol, echarge, cur,
   ind, rescon, volt, emr, work, flowld, force, fuelc, lux, magnf, mus, power,
-  prefx, shoe, press, speed, tque, visc, time;
+  prefx, shoe, press, speed, tque, visc, time, aboutk, maink;
 
 {$IFDEF WINDOWS}{$R Kelvin2.rc}{$ENDIF}
 
 {$R *.res}
 
 begin
-    Application.Title:='Kelvin';
+    Application.Title:='Chitwanix Kelvin';
   Application.Initialize;
   Application.CreateForm(Tkelvinm, kelvinm);
   Application.CreateForm(Tabk, abk);
